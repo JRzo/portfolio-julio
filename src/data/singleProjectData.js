1d1ba2faf999db6f32375b2b589f838247c8dd4a
Julio M Rodriguez <84085823+JRzo@users.noreply.github.com>
2025-05-09 00:12:42 -0400

    update

--- src/data/singleProjectData.js
+++ src/data/singleProjectData.js
@@ -1,129 +1,129 @@
-// Import images
-import Image1 from '../images/ui-project-1.jpg';
-import Image2 from '../images/web-project-2.jpg';
-import Image3 from '../images/mobile-project-2.jpg';
-import Image4 from '../images/mobile-project-1.jpg';
-import Image5 from '../images/web-project-1.jpg';
-import Image6 from '../images/ui-project-2.jpg';
-// Import icons
-import {
-	FiFacebook,
-	FiInstagram,
-	FiLinkedin,
-	FiTwitter,
-	FiYoutube,
-} from 'react-icons/fi';
+// // Import images
+// import Image1 from '../images/ui-project-1.jpg';
+// import Image2 from '../images/web-project-2.jpg';
+// import Image3 from '../images/mobile-project-2.jpg';
+// import Image4 from '../images/mobile-project-1.jpg';
+// import Image5 from '../images/web-project-1.jpg';
+// import Image6 from '../images/ui-project-2.jpg';
+// // Import icons
+// import {
+// 	FiFacebook,
+// 	FiInstagram,
+// 	FiLinkedin,
+// 	FiTwitter,
+// 	FiYoutube,
+// } from 'react-icons/fi';
 
-export const singleProjectData = {
-	ProjectHeader: {
-		title: 'SHAPIIE',
-		publishDate: 'Currently',
-		tags: 'UI / Frontend',
-	},
-	ProjectImages: [
-		{
-			id: 1,
-			title: 'Kabul Project Management UI',
-			img: Image1,
-		},
-		{
-			id: 2,
-			title: 'Kabul Project Management UI',
-			img: Image2,
-		},
-		{
-			id: 3,
-			title: 'Kabul Project Management UI',
-			img: Image3,
-		},
-	],
-	ProjectInfo: {
-		ClientHeading: 'About Client',
-		CompanyInfo: [
-			{
-				id: 1,
-				title: 'Name',
-				details: 'SHAPIIE',
-			},
-			{
-				id: 2,
-				title: 'Services',
-				details: 'Full Stack Development',
-			},
-		],
-		ObjectivesHeading: 'Objective',
-		ObjectivesDetails:
-			'Shapiie is a carefully curated online boutique offering a stylish selection of clothing and unique lifestyle items. Our clothing range features [mention a few style keywords, e.g., contemporary basics, bohemian pieces, minimalist designs] sourced for their quality and aesthetic. Beyond apparel, Shapiie presents a collection of thoughtfully chosen that complement modern living. ',
-		Technologies: [
-			{
-				title: 'Tools & Technologies',
-				techs: [
-					'HTML',
-					'CSS',
-					'JavaScript',
-					'Vue.js',
-					'TailwindCSS',
-					'AdobeXD',
-				],
-			},
-		],
-		ProjectDetailsHeading: 'Challenge',
-		ProjectDetails: [
-			{
-				id: 1,
-				details:
-					'Platform Selection and Integration: Choosing the right e-commerce platform (Shopify, WooCommerce, custom development) and integrating it seamlessly with other necessary tools (payment gateways, shipping providers, marketing automation) can be complex. Ensuring compatibility and smooth data flow is crucial.',
-			},
-			{
-				id: 2,
-				details:
-					'Website Performance and Scalability: Optimizing the website for speed and ensuring it can handle increasing traffic and product listings as Shapiie grows is vital. Slow loading times can lead to high bounce rates and lost sales',
-			},
-			{
-				id: 3,
-				details:
-					'Security: Protecting customer data (payment information, personal details) is paramount. Implementing robust security measures to prevent breaches and comply with data privacy regulations (like GDPR or CCPA) is a significant responsibility.',
-			},
-		],
-		SocialSharingHeading: 'Share This',
-		SocialSharing: [
-			{
-				id: 1,
-				name: 'Twitter',
-				icon: <FiTwitter />,
-				url: 'https://twitter.com/realstoman',
-			},
-			{
-				id: 4,
-				name: 'LinkedIn',
-				icon: <FiLinkedin />,
-				url: 'https://linkedin.com/',
-			},
-		],
-	},
-	RelatedProject: {
-		title: 'Related Projects',
-		Projects: [
-			{
-				id: 1,
-				title: 'Mobile UI',
-				img: Image4,
-			},
-			{
-				id: 2,
-				title: 'Web Application',
-				img: Image5,
-			},
-			{
-				id: 3,
-				title: 'UI Design',
-				img: Image6,
-			},
-			{
-				id: 4,
-				title: 'Kabul Mobile App UI',
-				img: Image3,
-			},
-		],
-	},
-};
+// export const singleProjectData = {
+// 	ProjectHeader: {
+// 		title: 'SHAPIIE',
+// 		publishDate: 'Currently',
+// 		tags: 'UI / Frontend',
+// 	},
+// 	ProjectImages: [
+// 		{
+// 			id: 1,
+// 			title: 'Kabul Project Management UI',
+// 			img: Image1,
+// 		},
+// 		{
+// 			id: 2,
+// 			title: 'Kabul Project Management UI',
+// 			img: Image2,
+// 		},
+// 		{
+// 			id: 3,
+// 			title: 'Kabul Project Management UI',
+// 			img: Image3,
+// 		},
+// 	],
+// 	ProjectInfo: {
+// 		ClientHeading: 'About Client',
+// 		CompanyInfo: [
+// 			{
+// 				id: 1,
+// 				title: 'Name',
+// 				details: 'SHAPIIE',
+// 			},
+// 			{
+// 				id: 2,
+// 				title: 'Services',
+// 				details: 'Full Stack Development',
+// 			},
+// 		],
+// 		ObjectivesHeading: 'Objective',
+// 		ObjectivesDetails:
+// 			'Shapiie is a carefully curated online boutique offering a stylish selection of clothing and unique lifestyle items. Our clothing range features [mention a few style keywords, e.g., contemporary basics, bohemian pieces, minimalist designs] sourced for their quality and aesthetic. Beyond apparel, Shapiie presents a collection of thoughtfully chosen that complement modern living. ',
+// 		Technologies: [
+// 			{
+// 				title: 'Tools & Technologies',
+// 				techs: [
+// 					'HTML',
+// 					'CSS',
+// 					'JavaScript',
+// 					'Vue.js',
+// 					'TailwindCSS',
+// 					'AdobeXD',
+// 				],
+// 			},
+// 		],
+// 		ProjectDetailsHeading: 'Challenge',
+// 		ProjectDetails: [
+// 			{
+// 				id: 1,
+// 				details:
+// 					'Platform Selection and Integration: Choosing the right e-commerce platform (Shopify, WooCommerce, custom development) and integrating it seamlessly with other necessary tools (payment gateways, shipping providers, marketing automation) can be complex. Ensuring compatibility and smooth data flow is crucial.',
+// 			},
+// 			{
+// 				id: 2,
+// 				details:
+// 					'Website Performance and Scalability: Optimizing the website for speed and ensuring it can handle increasing traffic and product listings as Shapiie grows is vital. Slow loading times can lead to high bounce rates and lost sales',
+// 			},
+// 			{
+// 				id: 3,
+// 				details:
+// 					'Security: Protecting customer data (payment information, personal details) is paramount. Implementing robust security measures to prevent breaches and comply with data privacy regulations (like GDPR or CCPA) is a significant responsibility.',
+// 			},
+// 		],
+// 		SocialSharingHeading: 'Share This',
+// 		SocialSharing: [
+// 			{
+// 				id: 1,
+// 				name: 'Twitter',
+// 				icon: <FiTwitter />,
+// 				url: 'https://twitter.com/realstoman',
+// 			},
+// 			{
+// 				id: 4,
+// 				name: 'LinkedIn',
+// 				icon: <FiLinkedin />,
+// 				url: 'https://linkedin.com/',
+// 			},
+// 		],
+// 	},
+// 	RelatedProject: {
+// 		title: 'Related Projects',
+// 		Projects: [
+// 			{
+// 				id: 1,
+// 				title: 'Mobile UI',
+// 				img: Image4,
+// 			},
+// 			{
+// 				id: 2,
+// 				title: 'Web Application',
+// 				img: Image5,
+// 			},
+// 			{
+// 				id: 3,
+// 				title: 'UI Design',
+// 				img: Image6,
+// 			},
+// 			{
+// 				id: 4,
+// 				title: 'Kabul Mobile App UI',
+// 				img: Image3,
+// 			},
+// 		],
+// 	},
+// };
